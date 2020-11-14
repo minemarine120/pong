92af21f5cfcf4abd5fc5300b63ca2dfda86fe220
Minemarine <yasmine.lanouar2019@gmail.com>
2020-11-14 16:28:33 +0100

    CP2

---
+++ New Unity Project/New Unity Project/Assets/ChekPoint/CheckPoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Sum(3,5));
+        Debug.Log(Prod(1,8));
+        Debug.Log(Div(9,2));
+        
+
+    }
+
+    private float Sum(float a, float b)
+    {
+        return a + b;
+    }
+    private float Prod (float a, float b)
+    {
+        return a + b;
+    }
+    private float Div (float a, float b)  
+    {
+        if (b == 0)
+        {
+            return 0;
+        }
+        else{
+            return a / b;
+        }
+    } 
+        
+}
+
+
+
+
